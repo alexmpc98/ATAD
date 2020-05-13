ba615adf154bf3f509f3de276b26eb7851868e22
alexmpc98 <48052322+alexmpc98@users.noreply.github.com>
2020-05-13 17:09:03 +0100

    Remoção Lab06
    Remoção Lab06

--- Labs/ATAD_LAB6/utils.h
+++
@@ -1,7 +0,0 @@
-#include <string.h>
-#include "listElem.h"
-#include "list.h"
-
-char ** split(char *string, int nFields, const char *delim);
-void importKahootFromFile(char *filename, PtList *listKR);
-void sortByAlphabeticalOrder(PtList *listKR);
